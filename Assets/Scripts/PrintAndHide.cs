c1fc8d95521761e010520ed5fd0914c5d3ce0856
yibo <Yibo.Hao-1@student.uts.edu.au>
2021-08-26 12:40:19 +1000

    90% commit

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
 
     int i = 3;
+
+    public Renderer rend;
 
     // Start is called before the first frame update
     void Start()
