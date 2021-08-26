2a5e88284bd552239fc681625043c773dd74a28f
yibo <Yibo.Hao-1@student.uts.edu.au>
2021-08-27 07:29:29 +1000

    100% complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -9,10 +9,12 @@ public class PrintAndHide : MonoBehaviour
 
     public Renderer rend;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    int random;
 
+    // Start is called before the first frame update
+    void Awake()
+    {
+        random = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -20,5 +22,15 @@ public class PrintAndHide : MonoBehaviour
     {
         i++;
         Debug.Log(gameObject.name + ":" + i);
+        //Debug.Log("random number: " + random);
+
+        if (i == 100 && GameObject.FindWithTag("Red") != null)
+        {
+            GameObject.FindWithTag("Red").SetActive(false);
+        }
+        if(i == random)
+        {
+            GameObject.FindWithTag("Blue").GetComponent<Renderer>().enabled = false;
+        }
     }
 }
