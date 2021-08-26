acbe8a0a9a42c8ddbe8e6eb92f877ed1be103fc1
yibo <Yibo.Hao-1@student.uts.edu.au>
2021-08-26 12:14:37 +1000

    70% complete

--- Assets/Scripts/LoadAssets.cs
+++ Assets/Scripts/LoadAssets.cs
@@ -6,12 +6,19 @@ public class LoadAssets : MonoBehaviour
 {
 
     public GameObject redObj;
+    [SerializeField]
+    private GameObject blueObj;
+
+    Vector3 redPos= new Vector3(2,0,0);
+    Vector3 bluePos = new Vector3(-2, 0, 0);
+    Quaternion rotation = new Quaternion(0, 0, 0, 0);
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(redObj, redPos, rotation);
+        Instantiate(blueObj, bluePos, rotation);
     }
 
     // Update is called once per frame
